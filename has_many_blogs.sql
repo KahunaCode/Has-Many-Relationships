e641bbd5b35b2c3fcdf57dc35db7000736aaf00b
James Moore <jameslmoore@gmail.com>
2017-07-26 21:46:49 -1000

    create users table

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -4,3 +4,15 @@ DROP USER if exists "has_many_user";
 
 CREATE USER has_many_user;
 
+DROP DATABASE if exists "has_many_blogs";
+
+CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
+
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+username VARCHAR(90) NOT NULL,
+first_name VARCHAR(90) DEFAULT NULL,
+last_name VARCHAR(90) DEFAULT NULL,
+created_at TIMESTAMP DEFAULT now(),
+updated_at TIMESTAMP DEFAULT now()
+);
