d8d678bfab4bc5f23f712acd52d2abdaf947f334
James Moore <jameslmoore@gmail.com>
2017-07-27 19:23:36 -1000

    more joins

--- joins.sql
+++ joins.sql
@@ -17,3 +17,24 @@ INNER JOIN users ON posts.user_id = users.id
 WHERE users.first_name LIKE 'Norene' AND
 users.last_name LIKE 'Schmitt';
 
+--5
+SELECT username FROM users WHERE created_at > '2015-01-01';
+
+--6
+SELECT posts.title, posts.content, users.username
+FROM posts
+INNER JOIN users ON posts.user_id = users.id
+WHERE users.created_at < '2015-01-01';
+
+--7
+SELECT comments.*, posts.title AS "Post Title"
+FROM comments
+INNER JOIN posts ON comments.post_id = posts.id;
+
+--8
+SELECT comments.*, posts.title AS "Post Title",
+posts.url AS "post_url",
+comments.body AS "comment_body"
+FROM comments
+INNER JOIN posts ON comments.post_id = posts.id
+WHERE posts.created_at < '2015-01-01';
